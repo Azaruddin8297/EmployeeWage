cdb1d5ec6139b9a02318fb60abd818ca79e52245
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-17 19:35:57 +0530

    By using Object

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeWageComputationProblem
+{
+    public class Grettings
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome tho the Employee wage computation program");
+            EmployeeCheck_UC1 uc1 = new EmployeeCheck_UC1();
+            uc1.EmployeeCheck();
+        }
+    }
+
+}
